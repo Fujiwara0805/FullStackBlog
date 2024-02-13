34399019ab475059a51b6bd75e5b9a997f33d5cc
Fujiwara0805 <tiki4091@gmail.com>
2024-02-13 21:43:06 +0900

    ✨ ブログ投稿機能の実装

--- app/blog/add/page.tsx
+++ app/blog/add/page.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
 
-function page() {
-  return <div>page</div>;
+/* エンドポイント処理 */
+const postBlog = async (
+  title: string | undefined,
+  description: string | undefined
+) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/blog`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, description }),
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return await res.json();
+  } catch (error) {
+    console.error("接続エラーです", error);
+  }
+};
+
+function PostBlog() {
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    toast.loading("投稿中です....", { id: "1" });
+    await postBlog(titleRef.current?.value, descriptionRef.current?.value);
+    toast.success("投稿に成功しました!!!", { id: "1" });
+    router.push("/");
+    router.refresh();
+  };
+
+  return (
+    <>
+      <Toaster />
+      <div className="w-full m-auto flex my-4">
+        <div className="flex flex-col justify-center items-center m-auto">
+          <p className="text-2xl text-slate-200 font-bold p-3">
+            ブログ新規作成 🚀
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={titleRef}
+              placeholder="タイトルを入力"
+              type="text"
+              className="rounded-md px-4 w-full py-2 my-2"
+            />
+            <textarea
+              ref={descriptionRef}
+              placeholder="記事詳細を入力"
+              className="rounded-md px-4 py-2 w-full my-2"
+            ></textarea>
+            <button className="font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100">
+              投稿
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default page;
+export default PostBlog;
