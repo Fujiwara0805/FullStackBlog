abe96aa5f18fcd1d943f481830804e99bc08c751
Fujiwara0805 <tiki4091@gmail.com>
2024-02-19 22:12:20 +0900

    ✨ 検索用Apiの作成及びSearchコンポーネントの実装

---
+++ app/components/elements/SearchBox/SearchBox.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState } from "react";
+
+function SearchBox() {
+  /* field */
+  const [query, setQuery] = useState("");
+
+  /* Blog検索処理 */
+  const handleSearch = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/blog/search`, {
+        method: "POST",
+      });
+      const data = await res.json();
+      return data.posts;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      />
+      <button
+        className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-blue-500 font-semibold"
+        onClick={handleSearch}
+      >
+        検索
+      </button>
+    </div>
+  );
+}
+
+export default SearchBox;
